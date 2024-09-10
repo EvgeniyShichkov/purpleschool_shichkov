662e460aaa10d0ed44df17b2be3059161a057731
евгений шичков <egshichkov@sberbank.ru>
2024-09-10 22:23:36 +0300

    2-dice

--- 1-map.js
+++ 1-map.js
@@ -9,15 +9,15 @@ const persons = [
 
 // 1
 let mapa  = new Map([persons]); 
-console.log([...mapa]) // => [{"id": 1,"name": "Вася"},{"id": 2,"name": "Петя"}]
+// console.log([...mapa])
 
 // 2
 let set = new Set(persons);
 const double = [...persons].findLast((item) => set.has(item))
 set.delete(double)
-console.log([...set]) // new Set([{ "id": 1,"name": "Вася"},{"id": 2,"name": "Петя"])
+// console.log([...set])
 
 // 3
 const uniquePerson = [...new Set(persons.map(item => persons.findLast(el => item.id !== el.id)))]
-console.log(uniquePerson) // => { id: 2, name: "Петя" }, { id: 1, name: "Вася" }
+// console.log(uniquePerson)
 
